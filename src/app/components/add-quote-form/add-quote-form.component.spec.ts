70dc5c93e74283334ca0093a37db19879b452037
Brenda-M <brendanjuguna1@gmail.com>
2020-03-28 23:25:30 +0300

    add styling to form

---
+++ src/app/components/add-quote-form/add-quote-form.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddQuoteFormComponent } from './add-quote-form.component';
+
+describe('AddQuoteFormComponent', () => {
+  let component: AddQuoteFormComponent;
+  let fixture: ComponentFixture<AddQuoteFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AddQuoteFormComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddQuoteFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
