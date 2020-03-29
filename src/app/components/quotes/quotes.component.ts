6c3197b9b0da9b51c9144fb65a5e61e1b123354a
Brenda-M <brendanjuguna1@gmail.com>
2020-03-29 19:01:11 +0300

    create function to highlight quote with the highest likes

--- src/app/components/quotes/quotes.component.ts
+++ src/app/components/quotes/quotes.component.ts
@@ -8,17 +8,32 @@ import { Quotes } from '../../models/quotes'
 })
 export class QuotesComponent implements OnInit {
   quotes = [
-    new Quotes(1,'Life can only be understood backwords; but it must be lived fowards', 'Soren Kierkegaard', 'Joe86', new Date(2019, 0, 6), 8, 0),
-    new Quotes(2, 'Beware of the barrenness of a busy life', 'Socrates', 'Bill Rogers', new Date(2016, 2,8), 16, 8),
-    new Quotes (3, 'Knowledge is knowing a tomato is a fruit; wisdom is not puting it in a fruit salad', 'Miles Kington', 'MO', new Date(2017, 1, 5), 11, 2),
-    new Quotes (4, 'Always borrow money from a pessimist. He won\'t expect it back', 'Oscar Wilde', 'Ian Stan', new Date (2020, 0, 1), 30, 9),
-    new Quotes (5, 'I doubt vodka is the answer. But it is worth a shot', 'Larry F', 'User95', new Date (2019, 6, 25),65, 3 )
+    new Quotes(1,'Life can only be understood backwords; but it must be lived fowards', 'Soren Kierkegaard', 'Joe86', new Date(2019, 0, 6), 0, 0),
+    new Quotes(2, 'Beware of the barrenness of a busy life', 'Socrates', 'Bill Rogers', new Date(2016, 2,8), 0, 0),
+    new Quotes (3, 'Knowledge is knowing a tomato is a fruit; wisdom is not puting it in a fruit salad', 'Miles Kington', 'MO', new Date(2017, 1, 5), 0, 0),
+    new Quotes (4, 'Always borrow money from a pessimist. He won\'t expect it back', 'Oscar Wilde', 'Ian Stan', new Date (2020, 0, 1), 0, 0),
+    new Quotes (5, 'I doubt vodka is the answer. But it is worth a shot', 'Larry F', 'User95', new Date (2019, 6, 25),0, 0)
   ]
+
+  prevNum:number
+  lastNum:number
+  counter:number
   
   moreDetails(index){
     this.quotes[index].showDetails=!this.quotes[index].showDetails
   }
-  
+
+  highestLikes(){
+    this.prevNum = 0
+    this.lastNum = 0
+    for (this.counter = 0; this.counter < this.quotes.length;this.counter++){
+      this.lastNum = this.quotes[this.counter].upvote;
+      if(this.lastNum > this.prevNum){
+        this.prevNum = this.lastNum
+      }
+    }
+    return this.prevNum 
+  }
   
   quoteDelete(isDelete, index){
     if (isDelete) {
@@ -31,6 +46,7 @@ export class QuotesComponent implements OnInit {
       
     }
   }
+  
 
   constructor() {
 
